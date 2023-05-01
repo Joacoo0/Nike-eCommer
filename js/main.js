60da261dd1aabf17234784f1e5cdf8e4f5fd6ee5
Joacoo0 <joaquin07david@gmail.com>
2023-04-30 21:46:44 -0300

    act5

--- js/main.js
+++ js/main.js
@@ -1,51 +1,107 @@
-const zapatillas = []
+const shopProductos = document.getElementById ("shop")
+const verCarrito = document.getElementById ("verCarrito")
+const modalContaner = document.getElementById("modal-content")
 
-alert ("!Hola¡ cual es su zapatilla nike favorita?")
-
-class zapatilla {
-  constructor (modelo, talle, edicion){
-    this.modelo = modelo
-    this.talle = talle
-    this.edicion = edicion
-  }
-}
-let continuar = true
-
-const buscarZapatilla = () =>{
-    let modelo
-    let talle
-    let edicion
-
-    do{
-        modelo = prompt("Cual modelo de zapatilla es su favorito")
-    }while(""){
-
+// mis productos
+const productos = [
+    {
+        id: 1,
+        nombre: "Air jordan 1 Mid",
+        precio: 200,
+        img: `img/jordan-rojas.webp`,
+    },
+    {
+        id: 2,
+        nombre: "Nike air jordan negras y blancas",
+        precio: 190,
+        img: `img/nike-air-jordan-1-negras-y-blancas.jpg`,
+    },
+    {
+        id: 3,
+        nombre: "Air jordan 1 Mid",
+        precio: 210,
+        img: `img/jordan-girse.jpg`,
+    },
+    {
+        id: 4,
+        nombre: "jordan 1 retro high white",
+        precio: 250,
+        img: `img/jordan-celestes.webp`,
     }
-    do{
-        talle = prompt("cual es su talle?")
-    }while(isNaN("")){
+]
 
-    }
-    do{
-        edicion = prompt("cual es su edicion favorita")
-    }while(""){
+let carrito = JSON.parse(localStorage.getItem("carrito")) || []
+//recorre productos y hace las tarjetas
+productos.forEach((product)=>{
+    let content = document.createElement("div") // crea un elemento html
+    content.className = "targeta" // le crea la clase targeta al elemtno html
+    content.innerHTML = ` 
+        <img src="${product.img}">
+        <h3>${product.nombre}</h3>
+        <p class = "precio">${product.precio}$ </p>
+    `
+    shopProductos.append(content)
+    let comprar = document.createElement("button") 
+    comprar.innerText = "Comprar"//NOMBRE
+    comprar.className = "comprar"//CLASS
 
-    }
+    content.append(comprar)
 
-    const zapa = new zapatilla (modelo,talle,edicion)
-    alert ("su eleccion fue:\n" + zapa.modelo + " " + "modelo\n" + zapa.talle + " " + "talle\n" + zapa.edicion + " " + "edicion\n")
+    comprar.addEventListener("click", () =>{
+        carrito.push({
+            id : product.id,
+            img : product.img,
+            nombre: product.nombre,
+            precio : product.precio,
+        })
+        console.log(carrito)
+        local ()
+    })
+})
+// funcion de ver el carrito
+verCarrito.addEventListener("click", () =>{
+    modalContaner.innerHTML = ""
+    modalContaner.style.display = "flex"
+    const modalHeader = document.createElement("div")
+    modalHeader.className = "modal-header"
+    modalHeader.innerHTML = `
+    <h1 class = "modal-header-title">Carrito</h1>
 
-    zapatillas.push(zapa)
-    console.log (zapatillas)
+    `
+    modalContaner.append(modalHeader)
+    const modalButton = document.createElement("button")
+    modalButton.innerText = "❌"
+    modalButton.className = "modal-header-button"
 
-    alert("Usted va agregando" + " " + zapatillas.length + " " + "zapatilla/s")
+    modalButton.addEventListener("click", () =>{
+        modalContaner.style.display = "none"
+    })
+
+    modalHeader.append(modalButton)
+
+
+    carrito.forEach((product)=>{
+        let carritoContent = document.createElement("div")
+        carritoContent.className = "modal-content2"
+        carritoContent.innerHTML = `
+        <img src= "${product.img}">
+        <h3>${product.nombre}</h3>
+        <p>${product.precio}$</p>
+        `
+        modalContaner.append(carritoContent)
+    
+    })
+
+    const total = carrito.reduce((acc, el) => acc + el.precio, 0)
+
+    const totalBuy = document.createElement("div")
+    totalBuy.className= "total-content"
+    totalBuy.innerHTML = `total a pagar: ${total} $`
+    modalContaner.append(totalBuy)
+
+})
+// local storage
+const local = () =>{
+    localStorage.setItem("carrito", JSON.stringify (carrito))
 }
 
-
-while(continuar){
-    buscarZapatilla()
-    continuar = confirm ("desea agregar otra zapatilla?")
-}
-
-alert ("has añadido" + " " + zapatillas.length + " zapatillas correctamente")
-alert ("!Terminaste¡ te puedes fijar en la consola tus zapatillas")
